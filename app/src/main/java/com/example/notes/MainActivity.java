e880aeb49eb7292529439d4f23ca4898906eff64
msuski12 <msuski@wisc.edu>
2021-10-18 22:13:36 -0500

    Added functional logout menu option

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.EditText;
 
@@ -12,13 +14,8 @@ public class MainActivity extends AppCompatActivity {
     public void clickFunction(View view) {
         EditText personName = (EditText)findViewById(R.id.editTextTextPersonName);
         String str = personName.getText().toString();
-        goToActivity2(str);
-        //Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
-    }
-
-    public void goToActivity2(String s) {
         Intent intent = new Intent(this, MainActivity2.class);
-        intent.putExtra("message", s);
+        intent.putExtra("message", str);
         startActivity(intent);
     }
 
--- app/src/main/java/com/example/notes/MainActivity2.java
+++ app/src/main/java/com/example/notes/MainActivity2.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
@@ -19,5 +22,25 @@ public class MainActivity2 extends AppCompatActivity {
         Intent intent = getIntent();
         String str = intent.getStringExtra("message");
         textView.setText(String.format("Welcome %s!", str));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId()==R.id.logout) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+        if (item.getItemId()==R.id.addNote) {
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
